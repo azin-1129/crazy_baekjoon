2de2988c36898241d5083f05a35feaa94af7254d
azin1129 <nuestiloveyou@gmail.com>
2024-10-08 18:13:07 +0900

    Solved: 23971 ZOAC4 17744KB 172ms

--- Java2024/October/ZOAC4.java
+++ Java2024/October/ZOAC4.java
@@ -7,27 +7,12 @@ public class ZOAC4 {
         // String path=System.getProperty("user.dir")+"/2024/10월/";
         Scanner sc=new Scanner(System.in);
 
-        int H=sc.nextInt();
-        int W=sc.nextInt();
-        int N=sc.nextInt();
-        int M=sc.nextInt();
+        double H=sc.nextInt();
+        double W=sc.nextInt();
+        double N=sc.nextInt();
+        double M=sc.nextInt();
 
-        int Wremain=W%(1+M);
-        int Hremain=H%(1+N);
-
-        int result=((H-Hremain)*(W-Wremain)) / ((1+M)*(1+N));
-
-        if(Wremain>0){
-            result+=(H/(1+N));
-            
-            if(Hremain>0){
-                result+=1;
-            }
-        }
-
-        if(Hremain>0){
-            result+=(W/(1+M));
-        }
+        int result=(int)(Math.ceil(W/(1+M))*Math.ceil(H/(1+N)));
 
         System.out.println(result);
     }
