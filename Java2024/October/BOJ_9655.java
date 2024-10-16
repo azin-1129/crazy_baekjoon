4e7f5db98fcd6db39d6372cebc369ab2130c49a1
azin1129 <nuestiloveyou@gmail.com>
2024-10-16 16:39:22 +0900

    Solved: 9655 돌 게임 17756KB 176ms

---
+++ Java2024/October/BOJ_9655.java
@@ -0,0 +1,18 @@
+package Java2024.October;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_9655 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+
+        int N=sc.nextInt();
+
+        if(N%2==0){
+            System.out.println("CY");
+        }else{
+            System.out.println("SK");
+        }
+    }
+}
