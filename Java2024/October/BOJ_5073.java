efaf6a85d49fbcb7851216867c51037a87f2bee9
azin1129 <nuestiloveyou@gmail.com>
2024-10-16 16:21:08 +0900

    Solved: 5073 삼각형과 세 변 14284KB 104ms

---
+++ Java2024/October/BOJ_5073.java
@@ -0,0 +1,51 @@
+package Java2024.October;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_5073 {
+    public static void main(String[] args) throws Exception{
+        String path=System.getProperty("user.dir")+"\\Java2024\\October\\";
+        BufferedReader br=new BufferedReader(new FileReader(path+"input5073.txt"));
+
+        while(true){
+            int[] numbers=Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            Arrays.sort(numbers);
+
+            int x=numbers[0];
+            int y=numbers[1];
+            int z=numbers[2];
+            
+            if((x+y+z)==0){
+                break;
+            }
+
+            if((x+y)<=z){
+                System.out.println("Invalid");
+                continue;
+            }
+
+            if(x==y){
+                if(x==z){
+                    System.out.println("Equilateral");
+                }else{
+                    System.out.println("Isosceles");
+                }
+            }else if(y==z){
+                if(x==z){
+                    System.out.println("Equilateral");
+                }else{
+                    System.out.println("Isosceles");
+                }
+            }else if(x==z){
+                if(y==z){
+                    System.out.println("Equilateral");
+                }else{
+                    System.out.println("Isosceles");
+                }
+            }else{
+                System.out.println("Scalene");
+            }
+        }
+    }
+}
