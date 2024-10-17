a902877a8ca1001d3c9ebbba48a22a7019af22cb
azin1129 <nuestiloveyou@gmail.com>
2024-10-17 20:04:43 +0900

    Solved: 2631 줄 세우기 14236KB 100ms

---
+++ Java2024/October/BOJ_2631.java
@@ -0,0 +1,34 @@
+package Java2024.October;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_2631 {
+    public static void main(String[] args) throws Exception {
+        String path=System.getProperty("user.dir")+"\\Java2024\\October\\";
+
+        BufferedReader br=new BufferedReader(new FileReader(path+"input2631.txt"));
+
+        int N=Integer.parseInt(br.readLine());
+
+        int[] nums=new int[N];
+        int[] lisDP=new int[N];
+
+        for(int i=0;i<N;i++){
+            nums[i]=Integer.parseInt(br.readLine());
+        }
+
+        for(int current=0;current<N;current++){
+            lisDP[current]=1;
+
+            for(int before=0;before<current;before++){
+                if(nums[current]>nums[before]){
+                    lisDP[current]=Math.max(lisDP[before]+1, lisDP[current]);
+                }
+            }
+        }
+
+        // System.out.println("lisDP 상태:"+Arrays.toString(lisDP));
+        System.out.println(N-Arrays.stream(lisDP).max().getAsInt());
+    }
+}
