b1339e1daf238ee10b7ea232e0ea5162b9bba807
azin-1129 <nuestiloveyou@gmail.com>
2024-10-23 17:05:28 +0900

    Solved: 15655. N과 M (6) 14536KB 108ms 조합,순열 이론숙지 필요

---
+++ Java2024/October/BOJ_15655.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+public class BOJ_15655 {
+    static int N,M;
+    static int[] numbers, result;
+    static boolean[] visited;
+    static StringBuilder sb=new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        String path=System.getProperty("user.dir");
+        BufferedReader br=new BufferedReader(new FileReader(path+"\\input15655.txt"));
+    
+        String[] inputs=br.readLine().split(" ");
+
+        N=Integer.parseInt(inputs[0]);
+        M=Integer.parseInt(inputs[1]);
+
+        visited=new boolean[N+1];
+        result=new int[M];
+        numbers=Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Arrays.sort(numbers);
+
+        combination(0, 0);
+
+        System.out.print(sb.toString());
+
+        br.close();
+    }
+    static void combination(int start, int depth){
+        if(depth==M){
+            for(int i=0;i<N;i++){
+                if(visited[i]){
+                    sb.append(numbers[i]);
+
+                    if(i!=(N-1)){
+                        sb.append(" ");
+                    }
+                }
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=start;i<N;i++){
+            if(!visited[i]){
+                visited[i]=true;
+                combination(i+1, depth+1);
+                visited[i]=false;
+            }
+        }
+    }
+}
