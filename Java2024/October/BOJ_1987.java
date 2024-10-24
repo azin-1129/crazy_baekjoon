44ee262893a1a0863951c485b381ffe871dd756d
azin1129 <nuestiloveyou@gmail.com>
2024-10-24 17:35:54 +0900

    Solved: 1987. 알파벳 295540KB 1436ms 아스키 변환이 중심

--- Java2024/October/BOJ_1987.java
+++ Java2024/October/BOJ_1987.java
@@ -2,71 +2,62 @@ import java.util.*;
 import java.io.*;
 
 public class BOJ_1987 {
-    static ArrayList<String> charList;
-    static String[][] map;
-    static boolean[][] visited;
-    static int R,C;
+    static int[][] map;
+    static int R,C,maxDepth;
+    static boolean[] visited;
     public static void main(String[] args) throws Exception {
         String path=System.getProperty("user.dir")+"\\";
         String fileName="input1987.txt";
         BufferedReader br=new BufferedReader(new FileReader(path+fileName));
 
-        charList=new ArrayList<>();
-
         String[] inputs=br.readLine().split(" ");
         R=Integer.parseInt(inputs[0]);
         C=Integer.parseInt(inputs[1]);
 
-        map=new String[R][C];
-        visited=new boolean[R][C];
+        map=new int[R][C];
 
         for(int x=0;x<R;x++){
-            inputs=br.readLine().split("");
+            String string=br.readLine();
             for(int y=0;y<C;y++){
-                map[x][y]=inputs[y];
+                map[x][y]=(int)string.charAt(y);
             }
         }
 
-        charList.add(map[0][0]);
-        System.out.println(BFS(0,0));
+        visited=new boolean[(int)'z'+1];
 
+        DFS(0,0, 1);
+
+        System.out.println(maxDepth);
         br.close();
     }
-    static int BFS(int x, int y){
+    static void DFS(int x, int y, int depth){
+        // System.out.println(x+","+y+" 방문중");
+        // System.out.println("alphabets 상태");
         int[] dx={-1,1,0,0};
         int[] dy={0,0,-1,1};
 
-        ArrayDeque<int[]> q=new ArrayDeque<>();
-
-        q.add(new int[]{x,y});
-
-        while(!q.isEmpty()){
-            int[] current=q.poll();
-
-            int currentX=current[0];
-            int currentY=current[1];
-
-            for(int d=0;d<4;d++){
-                int nextX=currentX+dx[d];
-                int nextY=currentY+dy[d];
-
-                if(nextX<0 || nextX>=R || nextY<0 || nextY>=C){
-                    continue;
-                }
-
-                if(visited[nextX][nextY]){
-                    continue;
-                }
-
-                visited[nextX][nextY]=true;
-
-                if(!charList.contains(map[nextX][nextY])){
-                    charList.add(map[nextX][nextY]);
-                    System.out.println(nextX+","+nextY+" "+map[nextX][nextY]+" 추가");
-                    q.add(new int[]{nextX, nextY});
-                }
-            }
+        visited[map[x][y]]=true;
+        if(depth>maxDepth){
+            maxDepth=depth;
         }
-        return charList.size();
+
+        for(int d=0;d<4;d++){
+            int nextX=x+dx[d];
+            int nextY=y+dy[d];
+
+            if(nextX<0 || nextX>=R || nextY<0 || nextY>=C){
+                continue;
+            }
+
+            if(visited[map[nextX][nextY]]){
+                continue;
+            }
+
+            DFS(nextX, nextY, depth+1);
+
+        }
+        
+        // return alphabets.size();
+        visited[map[x][y]] = false;
     }
 }
