86f89274ce8042e082d2da2ddde80c5e35538713
azin1129 <nuestiloveyou@gmail.com>
2024-10-29 19:02:22 +0900

    Study: HashMap에 대해

---
+++ Java2024/October/AboutHashMap.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class AboutHashMap {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map=new HashMap<>(10, 0.8f);
+
+        map.put(1, "One");
+        map.put(2, "Two");
+        map.put(3, "Three");
+
+        // System.out.println(map);
+
+        // map.remove(1);
+        // System.out.println(map);
+
+        System.out.println(map.get(1));
+        System.out.println(map.containsKey(2));
+        System.out.println(map.containsValue("Tree"));
+        System.out.println(map.containsValue("Three"));
+    }
+}
