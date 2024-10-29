68140f9bc9301820762639935ee9110713cb4610
azin1129 <nuestiloveyou@gmail.com>
2024-10-29 23:04:15 +0900

    Solved: 17219. 비밀번호 찾기 67492KB 1128ms

---
+++ Java2024/October/BOJ_17219.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class BOJ_17219 {
+    public static void main(String[] args) throws Exception {
+        String path=System.getProperty("user.dir");
+        BufferedReader br=new BufferedReader(new FileReader(path+"\\input17219.txt"));
+        String[] inputs;
+
+        inputs=br.readLine().split(" ");
+
+        int N=Integer.parseInt(inputs[0]); // 저장된 사이트 주소 수
+        int M=Integer.parseInt(inputs[1]); // 찾고자 하는 사이트 수
+
+        HashMap<String, String> hm=new HashMap<>(N, 0.8f);
+
+        for(int n=0;n<N;n++){
+            inputs=br.readLine().split(" ");
+
+            hm.put(inputs[0], inputs[1]);
+        }
+
+        String input;
+
+        for(int m=0;m<M;m++){
+            input=br.readLine();
+
+            System.out.println(hm.get(input));
+        }
+
+        br.close();
+    }
+}
