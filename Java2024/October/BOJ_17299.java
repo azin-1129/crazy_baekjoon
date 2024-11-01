b35adeffbb3317e3e3d00c28bd0c5f634e310b06
azin1129 <nuestiloveyou@gmail.com>
2024-11-01 20:45:46 +0900

    Solved: 17299. 오등큰수 204264KB 1080ms PriorityQueue 사용. Stack으로 해결한 코드 공부 요망

--- Java2024/October/BOJ_17299.java
+++ Java2024/October/BOJ_17299.java
@@ -6,8 +6,8 @@ class BOJ_17299{
         String filepath=System.getProperty("user.dir")+"\\";
         int bojNum=17299;
         BufferedReader br=new BufferedReader(new FileReader(filepath+"input"+bojNum+".txt"));
+        StringBuilder sb=new StringBuilder();
 
-        Stack<Integer> stack=new Stack<>();
         PriorityQueue<int[]> pq=new PriorityQueue<>(new Comparator<int[]>(){
             @Override
             public int compare(int[] o1, int[] o2){
@@ -18,20 +18,31 @@ class BOJ_17299{
         int N=Integer.parseInt(br.readLine());
         int[] numbers=Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int[] status=new int[1000001];
+        int[] result=new int[1000001];
 
-        for(int i=0;i<numbers.length;i++){
+        for(int i=0;i<N;i++){
             status[numbers[i]]+=1;
-            stack.push(numbers[i]);
         }
 
-        while(!stack.isEmpty()){
-            int current=stack.pop();
-            if(status[current]>=maximum){ // 이러면 현재 status 1일 때 2 3이면 답없
-                System.out.print("-1 ");
-            }else{
-                System.out.println();
+        for(int i=N-1;i>=0;i--){
+            while(!pq.isEmpty() && pq.peek()[0]<=status[numbers[i]]){
+                pq.poll(); // 최소 힙
             }
+
+            if(pq.isEmpty()){
+                result[i]=-1; // 현재보다 빈도 잦은 수 없음
+            }else{
+                result[i]=pq.peek()[1];
+            }
+
+            pq.offer(new int[]{status[numbers[i]], numbers[i]});
         }
+
+        for(int i=0;i<N;i++){
+            sb.append(result[i]+" ");
+        }
+
+        System.out.print(sb);
 
         br.close();
     }
