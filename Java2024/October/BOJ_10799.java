5ea6145733799d7f574b83fb319e3bbedbcd6d6a
azin1129 <nuestiloveyou@gmail.com>
2024-11-01 17:26:22 +0900

    Sovled: 10799. 쇠막대기 17360KB 180ms 레이저 판별이 관건. 단순 idx로 해도 될 듯

---
+++ Java2024/October/BOJ_10799.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+class BOJ_10799{
+    public static void main(String[] args) throws Exception{
+        String filepath=System.getProperty("user.dir")+"\\";
+        int bojNum=10799;
+        BufferedReader br=new BufferedReader(new FileReader(filepath+"input"+bojNum+".txt"));
+        int result=0;
+        
+        String status=br.readLine();
+
+        status=status.replace("()","x");
+
+        Stack<Character> stack=new Stack<>();
+
+        for(int i=0;i<status.length();i++){
+            char stat=status.charAt(i);
+
+            if(stat=='x'){
+                result+=stack.size();
+            }else if(stat==')'){
+                stack.pop();
+                result+=1;
+            }else{
+                stack.push(stat);
+            }
+        }
+
+        System.out.println(result);
+
+        br.close();
+    }
+}
