5af658af7d7bbf59297ee0b24e4cd7c1be45a9c8
azin1129 <nuestiloveyou@gmail.com>
2024-12-17 16:12:41 +0900

    Solved+: 9934. 완전 이진 트리 Input을 활용해 재귀적으로 nodeLayer에 depth 기반 누적

--- Java2024/December/Code/BOJ_9934.java
+++ Java2024/December/Code/BOJ_9934.java
@@ -1,112 +1,13 @@
 package Code;
 
 import java.util.*;
+
 import java.io.*;
 
-class Node{
-    int data;
-    Node left;
-    Node right;
-
-    public Node(int data){
-        this.data=data;
-    }
-}
-
-class BinaryTree{
-    Node root;
-
-    void createNode(int rootData, int leftData, int rightData){
-        if(root==null){
-            root=new Node(rootData);
-
-            if(leftData!=-1){
-                root.left=new Node(leftData);
-            }
-            if(rightData!=-1){
-                root.right=new Node(rightData);
-            }
-        }else{
-            searchNode(root, rootData, leftData, rightData);
-        }
-    }
-
-    void searchNode(Node node, int rootData, int leftData, int rightData){
-        if(node==null){
-            return;
-        }else if(node.data==rootData){
-            if(leftData!=-1){
-                root.left=new Node(leftData);
-            }
-            if(rightData!=-1){
-                root.right=new Node(rightData);
-            }
-        }else{
-            searchNode(node.left, rootData, leftData, rightData);
-            searchNode(node.right, rootData, leftData, rightData);
-        }
-    }
-
-    void preOrder(Node node){
-        if(node!=null){
-            System.out.println(node.data);
-            if(node.left!=null){
-                preOrder(node.left);
-            }
-            if(node.right!=null){
-                preOrder(node.right);
-            }
-        }
-    }
-
-    void inOrder(Node node){
-        if(node!=null){
-            if(node.left!=null){
-                preOrder(node.left);
-            }
-            System.out.println(node.data);
-            if(node.right!=null){
-                preOrder(node.right);
-            }
-        }
-    }
-
-    void postOrder(Node node){
-        if(node!=null){
-            if(node.left!=null){
-                preOrder(node.left);
-            }
-            System.out.println(node.data);
-            if(node.right!=null){
-                preOrder(node.right);
-            }
-        }
-    }
-
-    void levelOrder(){
-        this.levelOrder(root);
-    }
-
-    void levelOrder(Node node){
-        Queue<Node> q=new LinkedList<>();
-        q.add(node);
-
-        while(!q.isEmpty()){
-            Node current=q.poll();
-
-            System.out.println(current.data+" ");
-
-            if(current.left!=null){
-                q.add(current.left);
-            }
-            if(current.left!=null){
-                q.add(current.right);
-            }
-        }
-    }
-}
-
 class BOJ_9934{
+    static List<List<Integer>> nodeLayers;
+    static int[] nodes;
+    static int nodeCount,h;
     public static void main(String[] args) throws Exception{
         String filepath=System.getProperty("user.dir")+"\\Input\\";
         int bojNum=9934;
@@ -114,13 +15,38 @@ class BOJ_9934{
 
         int K=Integer.parseInt(br.readLine());
 
-        int[] weights=Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        nodeCount=(int)Math.pow(2,K)-1; // 노드는 2**k-1개
 
-        BinaryTree binaryTree=new BinaryTree();
+        nodes=Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
+        h=(int)(Math.log(nodeCount+1)/Math.log(2)); // 포화이진트리 높이
 
-        int[] temp=new int[3];
+        nodeLayers=new ArrayList<>();
 
+        for(int i=0;i<h;i++){
+            nodeLayers.add(new ArrayList<>());
+        }
+        
+        solve(0, nodeCount-1, 0);
+
+        for(int i=0;i<h;i++){
+            for(int node:nodeLayers.get(i)){
+                System.out.print(node+" ");
+            }
+            System.out.println();
+        }
         br.close();
+    }
+    static void solve(int start, int end, int depth){
+        if(start>end){
+            return;
+        }
+
+        int middle=(start+end)/2;
+
+        nodeLayers.get(depth).add(nodes[middle]);
+
+        solve(start, middle-1, depth+1);
+        solve(middle+1, end, depth+1);
     }
 }
