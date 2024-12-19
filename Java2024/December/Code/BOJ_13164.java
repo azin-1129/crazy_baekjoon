76fbc9de627dc3e64fd432ddccc0c536deeea0f2
azin1129 <nuestiloveyou@gmail.com>
2024-12-19 20:33:46 +0900

    Sovled+: 13164. 행복 유치원 데이터 정렬엔 퀵 정렬 사용했음. N-K번 그루핑이 핵심

--- Java2024/December/Code/BOJ_13164.java
+++ Java2024/December/Code/BOJ_13164.java
@@ -25,7 +25,6 @@ class QuickSort13164{
         int pivot=arr[left];
 
         while(left<right){
-            System.out.println("while1");
             while(arr[right]>pivot && left<right){
                 right--;
             }
@@ -63,16 +62,22 @@ class BOJ_13164{
         // 조마다 티셔츠 비용은 키 최대-키 최소 만큼 든다.
         // 이때 최소 티셔츠 비용 합계는?
 
+        int[] diff=new int[N-1];
+
+        // 인접한 애기들 키 차이 리스트 뽑기
+        for(int i=0;i<heights.length-1;i++){
+            diff[i]=heights[i+1]-heights[i];
+        }
+
         QuickSort13164 qs=new QuickSort13164();
 
-        qs.quickSort(heights);
-
-        System.out.println(Arrays.toString(heights));
+        qs.quickSort(diff);
 
         int result=0;
 
-        for(int i=0;i<heights.length/K;i++){
-            result+=(heights[i+K-1]-heights[i]);
+        // 그루핑
+        for(int i=0;i<N-K;i++){
+            result+=diff[i];
         }
 
         System.out.println(result);
