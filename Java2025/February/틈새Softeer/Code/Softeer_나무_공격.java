1f2cec98d096da90f80530b6760c6a74e3af91ce
azin1129 <nuestiloveyou@gmail.com>
2025-02-04 10:48:16 +0900

    Sovled: Softeer.나무 공격/구현

---
+++ Java2025/February/틈새Softeer/Code/Softeer_나무_공격.java
@@ -0,0 +1,62 @@
+package Code;
+
+import java.io.*;
+import java.util.*;
+
+public class Softeer_나무_공격 {
+    static int N,M;
+    static int[][] grid;
+    public static void main(String[] args) throws Exception{
+        String filepath=System.getProperty("user.dir")+"/Input/";
+        String probName="나무_공격";
+
+        BufferedReader br=new BufferedReader(new FileReader(filepath+probName+".txt"));
+        StringTokenizer st;
+
+        st=new StringTokenizer(br.readLine());
+
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
+
+        grid=new int[N][M];
+
+        for(int i=0;i<N;i++){
+            st=new StringTokenizer(br.readLine());
+            for(int j=0;j<M;j++){
+                grid[i][j]=Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i=0;i<2;i++){
+            st=new StringTokenizer(br.readLine());
+            int L=Integer.parseInt(st.nextToken())-1;
+            int R=Integer.parseInt(st.nextToken())-1;
+
+            attack(L, R);
+        }
+
+        System.out.println(countSurvivor());
+    }
+    static void attack(int L, int R){
+        for(int x=L;x<=R;x++){
+            for(int y=0;y<M;y++){
+                if(grid[x][y]==1){
+                    grid[x][y]=0;
+                    break;
+                }
+            }
+        }
+    }
+    static int countSurvivor(){
+        int count=0;
+        for(int x=0;x<N;x++){
+            for(int y=0;y<M;y++){
+                if(grid[x][y]==1){
+                    count+=1;
+                }
+            }
+        }
+
+        return count;
+    }
+}
