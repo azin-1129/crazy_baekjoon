3a6c0e6524cda901142cab2fa33628963351fe08
azin1129 <nuestiloveyou@gmail.com>
2025-03-20 15:58:09 +0900

    Solved+: 1774. 우주신과의 교감/프림은 아닌거 같ㅌ고.. 뭘까/25660KB 240ms

--- Java2025/March/Code/BOJ_1774.java
+++ Java2025/March/Code/BOJ_1774.java
@@ -2,8 +2,6 @@ package Code;
 
 import java.util.*;
 
-import org.w3c.dom.Node;
-
 import java.io.*;
 
 class BOJ_1774{
@@ -15,10 +13,18 @@ class BOJ_1774{
             this.idx=idx;
             this.weight=weight;
         }
+
+        public void setWeight(double weight){
+            this.weight=weight;
+        }
+
+        @Override
+        public String toString(){
+            return idx+"번째 node의 weight는 "+weight+" 입니다.";
+        }
     }
     static int N;
-    static List<List<Node1774>> graph;
-    static List<List<Integer>> setUp;
+    static double[][] distances;
     public static void main(String[] args) throws Exception{
         String filepath=System.getProperty("user.dir")+"\\Input\\";
         int bojNum=1774;
@@ -30,57 +36,50 @@ class BOJ_1774{
         int M=Integer.parseInt(st.nextToken());
 
         int[][] positions=new int[N+1][2];
-        int start=0;
+        distances=new double[N+1][N+1];
+
         for(int i=1;i<=N;i++){
             st=new StringTokenizer(br.readLine());
             int x=Integer.parseInt(st.nextToken());
             int y=Integer.parseInt(st.nextToken());
             positions[i][0]=x;
             positions[i][1]=y;
-
-            if(x==y){
-                start=x;
-            }
         }
 
-        graph=new ArrayList<>();
-        graph.add(new ArrayList<>());
         for(int x=1;x<=N;x++){
-            graph.add(new ArrayList<>());
             for(int y=1;y<=N;y++){
-                graph.get(x).add(new Node1774(y, calcDistance(positions[x], positions[y])));
+                distances[x][y]=calcDistance(positions[x], positions[y]);
             }
         }
 
-        double setUpValue=0;
-        setUp=new ArrayList<>();
-        for(int i=0;i<=N;i++){
-            setUp.add(new ArrayList<>());
-        }
+        // 이미 연결된 edge
         for(int i=0;i<M;i++){
             st=new StringTokenizer(br.readLine());
             int x=Integer.parseInt(st.nextToken());
             int y=Integer.parseInt(st.nextToken());
-            setUpValue+=graph.get(x).get(y-1).weight;
-            System.out.println("셋업 값을 계산합니다:"+setUpValue);
-            setUp.get(x).add(y);
+
+            // 이미 연결된 곳은 weight 0으로 처리
+            distances[x][y]=0;
+            distances[y][x]=0;
         }
 
-        System.out.println(prim(start)-setUpValue);
-        // Math.round(result*100)/100.0;
+        // System.out.printf("%.2f", Math.round(prim(1)*100)/100.0);
+        System.out.printf("%.2f", prim(1));
         br.close();
     }
     public static double prim(int start){
         boolean[] visited=new boolean[N+1];
+        double[] minDist=new double[N+1];
+        Arrays.fill(minDist, Double.MAX_VALUE);
         PriorityQueue<Node1774> pq=new PriorityQueue<>(new Comparator<Node1774>(){
             @Override
             public int compare(Node1774 n1, Node1774 n2){
-                return (int) (n1.weight-n2.weight);
+                return Double.compare(n1.weight,n2.weight);
             }
         });
-
+        
+        minDist[start]=0;
         pq.offer(new Node1774(start, 0));
-
         double result=0;
         while(!pq.isEmpty()){
             Node1774 currentNode=pq.poll();
@@ -92,25 +91,17 @@ class BOJ_1774{
             }
 
             visited[currentNodeIdx]=true;
-            System.out.println(currentNodeIdx+"를 등록했어요.");
             result+=currentNodeWeight;
+            minDist[currentNodeIdx]=0;
 
-            int flag=0;
-            System.out.println("current Node:"+currentNodeIdx);
-            System.out.println("셋업 정보:"+setUp.get(currentNodeIdx));
-            for(Node1774 nextNode:graph.get(currentNodeIdx)){
-                if(setUp.get(currentNodeIdx).contains(nextNode.idx)){
-                    System.out.println(currentNodeIdx+"에서 "+nextNode.idx+"로 가는 경로가 이미 존재해요. 패스합니다.");
-                    pq.offer(nextNode);
-                    flag=1;
-                    break;
+            for(int nextNodeIdx=1;nextNodeIdx<=N;nextNodeIdx++){
+                if(visited[nextNodeIdx]){
+                    continue;
                 }
-            }
-            if(flag==0){
-                for(Node1774 nextNode:graph.get(currentNodeIdx)){
-                    if(!visited[nextNode.idx]){
-                        pq.offer(nextNode);
-                    }
+
+                if((minDist[currentNodeIdx]+distances[currentNodeIdx][nextNodeIdx])<minDist[nextNodeIdx]){
+                    minDist[nextNodeIdx]=minDist[currentNodeIdx]+distances[currentNodeIdx][nextNodeIdx];
+                    pq.offer(new Node1774(nextNodeIdx, minDist[nextNodeIdx]));
                 }
             }
         }
