815a4d7373e213bc8a6247fd800c1cb1fe6af53e
azin1129 <nuestiloveyou@gmail.com>
2025-04-01 20:33:10 +0900

    Solved: 11718. 그대로 출력하기/단계별 전용/14348KB 108ms

--- Java2025/April/Code/BOJ_11718.java
+++ Java2025/April/Code/BOJ_11718.java
@@ -8,8 +8,20 @@ class BOJ_11718{
         String filepath=System.getProperty("user.dir")+"\\Input\\";
         int bojNum=11718;
         BufferedReader br=new BufferedReader(new FileReader(filepath+"input"+bojNum+".txt"));
+        StringBuilder sb=new StringBuilder();
 
-        
+        String s;
+
+        while(true){
+            s=br.readLine();
+            if(s==null){
+                break;
+            }
+            sb.append(s+"\n");
+        }
+        sb.deleteCharAt(sb.length()-1);
+        System.out.println(sb);
+
         br.close();
     }
 }
