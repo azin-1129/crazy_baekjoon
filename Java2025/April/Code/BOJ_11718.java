341022d1206f59f31a918b9562be716e3af58dca
azin1129 <nuestiloveyou@gmail.com>
2025-04-01 20:29:28 +0900

    Chore: 4월 패키지 생성

---
+++ Java2025/April/Code/BOJ_11718.java
@@ -0,0 +1,15 @@
+package Code;
+
+import java.util.*;
+import java.io.*;
+
+class BOJ_11718{
+    public static void main(String[] args) throws Exception{
+        String filepath=System.getProperty("user.dir")+"\\Input\\";
+        int bojNum=11718;
+        BufferedReader br=new BufferedReader(new FileReader(filepath+"input"+bojNum+".txt"));
+
+        
+        br.close();
+    }
+}
