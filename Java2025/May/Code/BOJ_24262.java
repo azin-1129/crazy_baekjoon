b161dd3d37b6f01e7fb8459935c361c7a6f75b8f
azin1129 <nuestiloveyou@gmail.com>
2025-05-14 19:25:20 +0900

    Solved: 24262. 알고리즘 수업 - 알고리즘의 수행 시간 1/단계별 전용/14116KB 100ms

--- Java2025/May/Code/BOJ_24262.java
+++ Java2025/May/Code/BOJ_24262.java
@@ -3,12 +3,15 @@ package Code;
 import java.util.*;
 import java.io.*;
 
-class BOJ_문제번호{
+class BOJ_24262{
     public static void main(String[] args) throws Exception{
         String filepath=System.getProperty("user.dir")+"\\Input\\";
-        int bojNum=0;
+        int bojNum=24262;
         BufferedReader br=new BufferedReader(new FileReader(filepath+"input"+bojNum+".txt"));
 
+        int n=Integer.parseInt(br.readLine());
+        System.out.println(1);
+        System.out.println(0);
         br.close();
     }
 }
