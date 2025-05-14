bce402ba5c569b601d0dbc1c4e51fa9a642c5d91
azin1129 <nuestiloveyou@gmail.com>
2025-05-14 19:25:48 +0900

    Solved: 24263. 알고리즘 수업 - 알고리즘의 수행 시간 2/단계별 전용/14232KB 104ms

---
+++ Java2025/May/Code/BOJ_24263.java
@@ -0,0 +1,15 @@
+package Code;
+
+import java.util.*;
+import java.io.*;
+
+class BOJ_24263{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+
+        System.out.println(n);
+        System.out.println(1);
+        br.close();
+    }
+}
