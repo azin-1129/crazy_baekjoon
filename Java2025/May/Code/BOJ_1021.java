9726ed0ff82a38930f166fe16201da533d0c70ae
azin1129 <nuestiloveyou@gmail.com>
2025-06-01 20:38:50 +0900

    Solved: 1021. 회전하는 큐 바킹독 .0x07 14216KB 104ms

--- Java2025/May/Code/BOJ_1021.java
+++ Java2025/May/Code/BOJ_1021.java
@@ -5,8 +5,10 @@ import java.util.*;
 
 class BOJ_1021{
     static Deque<Integer> dq;
+    static int[] idxs;
     static int calc2Count=0;
     static int calc3Count=0;
+    static int M;
     public static void main(String[] args) throws Exception {
         String filepath=System.getProperty("user.dir")+"\\Input\\";
         int bojNum=1021;
@@ -15,22 +17,19 @@ class BOJ_1021{
 
         st=new StringTokenizer(br.readLine(), " ");
         int N=Integer.parseInt(st.nextToken());
-        int M=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
 
         dq=new ArrayDeque<>();
         for(int i=1;i<=N;i++){
             dq.offer(i);
         }
 
-        int[] idxs=new int[M];
+        idxs=new int[M];
         st=new StringTokenizer(br.readLine(), " ");
         for(int i=0;i<M;i++){
             idxs[i]=Integer.parseInt(st.nextToken())-1; // 번호가 1부터 주어졌으므로 -1
         }
 
-        // int test=Integer.valueOf(idxs[0]);
-        // idxs[0]-=1;
-        // System.out.println(test+", origin:"+idxs[0]);
         for(int i=0;i<M;i++){
             // 연산 시, i+1<M idx 배열에 대한 연산이 필요
             int idx=idxs[i];
@@ -43,45 +42,37 @@ class BOJ_1021{
             System.out.println("idxs 상태:"+Arrays.toString(idxs));
             System.out.println(dq);
             if(middle>idx){ // 좌
-                // 현 idx-1번 연산한 후, calc1
-                // 연산 2
                 for(int j=0;j<idx;j++){
                     calc2();
                 }
-                calc1();
-                // 인덱스 값 연산
-                // 연산을 하지 않아도 갱신될 수 있겠다.
+                calc1(i);
 
-                for(int j=i;j<M;j++){
+                for(int j=i+1;j<M;j++){
                     idxs[j]=idxs[j]-idx;
                     if(idxs[j]<0){
                         idxs[j]=(dqSize+idxs[j]);
                     }
                 }
             }else if(middle<idx){ // 우
-                // dq.size()-현 idx번 연산한 후, calc1
-                // 연산 3
-                for(int j=0;j<=(dqSize-idx);j++){
+                for(int j=0;j<(dqSize-idx);j++){
                     calc3();
                 }
-                calc1();
-                // 인덱스 값 연산
-                for(int j=i;j<M;j++){
-                    idxs[j]=idxs[j]+(dqSize-idx)-1; // calc1() 때문에 -1
+                calc1(i);
+
+                for(int j=i+1;j<M;j++){
+                    idxs[j]=idxs[j]+(dqSize-idx);
                     if(idxs[j]>=dqSize){
                         idxs[j]=(idxs[j]-dqSize);
                     }
                 }
             }else{ // 가운데라면, 2번 연산 수행
-                                // 현 idx-1번 연산한 후, calc1
-                // 연산 2
                 for(int j=0;j<idx;j++){
                     calc2();
                 }
-                calc1();
+                calc1(i);
 
-                for(int j=i;j<M;j++){
-                    idxs[j]=idxs[j]-idx; // calc1() 때문에 -1
+                for(int j=i+1;j<M;j++){
+                    idxs[j]=idxs[j]-idx;
                     if(idxs[j]<0){
                         idxs[j]=(dqSize+idxs[j]);
                     }
@@ -94,8 +85,12 @@ class BOJ_1021{
         System.out.println(calc2Count+calc3Count);
         br.close();
     }
-    static void calc1(){
+    static void calc1(int idx){
         dq.pollFirst();
+
+        for(int i=idx;i<M;i++){
+            idxs[i]-=1;
+        }
     }
     static void calc2(){
         int x=dq.pollFirst();
