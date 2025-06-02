fabf69bc7e339ef0ed06bb169e025d75ea0168ed
azin1129 <nuestiloveyou@gmail.com>
2025-06-02 19:29:13 +0900

    Solved: 3986. 좋은 단어 바킹독 0x08 57652KB 484ms

--- Java2025/June/Code/BOJ_3986.java
+++ Java2025/June/Code/BOJ_3986.java
@@ -6,9 +6,34 @@ import java.io.*;
 class BOJ_문제번호{
     public static void main(String[] args) throws Exception{
         String filepath=System.getProperty("user.dir")+"\\Input\\";
-        int bojNum=0;
+        int bojNum=3986;
         BufferedReader br=new BufferedReader(new FileReader(filepath+"input"+bojNum+".txt"));
 
+        int result=0;
+        int N=Integer.parseInt(br.readLine());
+        for(int i=0;i<N;i++){
+            Stack<String> stack=new Stack<>();
+            String[] input=br.readLine().split("");
+            // System.out.println(Arrays.toString(input));
+
+            for(String s:input){
+                if(stack.isEmpty()){
+                    stack.push(s);
+                    continue;
+                }else{
+                    if(stack.peek().equals(s)){
+                        stack.pop();
+                    }else{
+                        stack.push(s);
+                    }
+                }
+            }
+
+            if(stack.isEmpty()){
+                result+=1;
+            }
+        }
+        System.out.println(result);
         br.close();
     }
 }
