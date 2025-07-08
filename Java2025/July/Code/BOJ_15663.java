3892b1d7c58c1307340273a270ebda8932ebaa92
azin1129 <nuestiloveyou@gmail.com>
2025-07-08 21:34:16 +0900

    Solved+: 15663. N과 M (9) 바킹독 0x0C 27448KB 248ms

--- Java2025/July/Code/BOJ_15663.java
+++ Java2025/July/Code/BOJ_15663.java
@@ -7,8 +7,6 @@ class BOJ_15663{
     static int[] result, nums;
     static boolean[] visited;
     static int N, M;
-    static int[][][] check;
-    static int start;
     static StringBuilder sb=new StringBuilder();
     public static void main(String[] args) throws Exception{
         String filepath=System.getProperty("user.dir")+"\\Input\\";
@@ -28,7 +26,6 @@ class BOJ_15663{
             nums[i]=Integer.parseInt(st.nextToken());
             max=Math.max(nums[i], max);
         }
-        check=new int[max+1][max+1][M];
 
         Arrays.sort(nums);
         perm(0);
@@ -38,41 +35,23 @@ class BOJ_15663{
         br.close();
     }
     static void perm(int depth){
-        // System.out.println("perm?");
         if(depth>=M){
-            if(check[start][result[M-1]][depth-1]==2){ // 이미 봤던 조합
-                return;
-            }
             for(int i=0;i<M;i++){
                 sb.append(result[i]+" ");
             }
             sb.deleteCharAt(sb.length()-1);
             sb.append("\n");
-            // System.out.println(Arrays.toString(result).replaceAll("[\\[\\],]",""));
             return;
         }
-
-        // perm(0)
-        // visited[0]=true;
-        // result[0]=nums[0];
-        // perm(1);
-        // visited[1]=true;
-        // result[1]=nums[1];
-        // perm(2);
-
-        // check가 3차원 배열, check[N][N][M]
-        // 1 depth부터 원소 선정 시작이니 check[1depth원소][다음선택한원소][depth] 갱신 필요
+        int tmp=0;
         for(int i=0;i<N;i++){
-            // System.out.println("perm?");
             if(!visited[i]){
+                if(nums[i]==tmp){
+                    continue;
+                }
                 visited[i]=true;
                 result[depth]=nums[i];
-
-                if(depth==0){
-                    start=nums[i]; // 1depth로 선정될 원소
-                }else{ // 2depth 원소 선택 시작
-                    check[start][nums[i]][depth]+=1;
-                }
+                tmp=nums[i];
                 perm(depth+1);
                 visited[i]=false;
             }
