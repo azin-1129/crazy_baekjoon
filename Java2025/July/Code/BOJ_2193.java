1c24537edb6ef61706f598e81abd2eab6b1e6d70
azin1129 <nuestiloveyou@gmail.com>
2025-07-30 10:29:06 +0900

    Solved+: 2193. 이천수 바킹독 0x10 14268KB 112ms

---
+++ Java2025/July/Code/BOJ_2193.java
@@ -0,0 +1,29 @@
+package Code;
+
+import java.util.*;
+import java.io.*;
+
+class BOJ_2193{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        
+        int N=Integer.parseInt(br.readLine());
+        if(N==1){
+            System.out.println(1);
+        }else if(N==2){
+            System.out.println(1);
+        }else{
+            long[] dp=new long[N+1];
+            dp[1]=1;
+            dp[2]=1;
+            
+            for(int i=3;i<=N;i++){
+                dp[i]=dp[i-1]+dp[i-2];
+            }
+        
+            System.out.println(dp[N]);
+        }
+        
+        br.close();
+    }
+}
