52d09398fbbefd70ad02c619f6720bc367f532c5
azin1129 <nuestiloveyou@gmail.com>
2025-08-04 21:01:41 +0900

    Solved: 11053. 가장 긴 증가하는 부분 수열 바킹독 0x10 이진탐색 오류... 14344KB 116ms

--- Java2025/August/Code/BOJ_11053.java
+++ Java2025/August/Code/BOJ_11053.java
@@ -4,42 +4,44 @@ import java.util.*;
 import java.io.*;
 
 class BOJ_11053{
-    static int[] numbers;
+    static int[] dp;
     public static void main(String[] args) throws Exception{
         String filepath=System.getProperty("user.dir")+"\\Input\\";
         int bojNum=11053;
         BufferedReader br=new BufferedReader(new FileReader(filepath+"input"+bojNum+".txt"));
 
         int N=Integer.parseInt(br.readLine());
-        int[] dp=new int[N];
-        numbers=new int[N];
-        int idx=0;
+        dp=new int[N];
+        int[] numbers=new int[N];
+        int idx=1;
         StringTokenizer st=new StringTokenizer(br.readLine(), " ");
         for(int i=0;i<N;i++){
             numbers[i]=Integer.parseInt(st.nextToken());
         }
         // System.out.println("numbers:"+Arrays.toString(numbers));
-        for(int i=0;i<N;i++){
+        dp[0]=numbers[0];
+        for(int i=1;i<N;i++){
             System.out.println("현재 idx:"+idx);
-            if(dp[idx]<numbers[i]){
+            if(dp[idx-1]<numbers[i]){
                 dp[idx++]=numbers[i];
-            }else if(dp[idx]>numbers[i]){
+            }else{
                 int foundIdx=binarySearch(0, idx-1, numbers[i]);
                 System.out.println("이분탐색 idx:"+foundIdx);
                 dp[foundIdx]=numbers[i];
             }
-            System.out.println(Arrays.toString(dp));
+            // System.out.println(Arrays.toString(dp));
         }
 
+        System.out.println(Arrays.toString(dp));
         System.out.println(idx);
         br.close();
     }
     static int binarySearch(int start, int end, int value){
-        while(start<end){
+        while(start<=end){
             int middle=(start+end)/2;
-            if(numbers[middle]<value){
+            if(dp[middle]>value){
                 end=middle-1;
-            }else if(numbers[middle]>value){
+            }else if(dp[middle]<value){
                 start=middle+1;
             }else{
                 return middle;
