9b883d3652988d6cc67ea9a3422d07fc14e17aaf
azin1129 <nuestiloveyou@gmail.com>
2025-08-04 19:20:57 +0900

    Chore: August 디렉터리 생성

---
+++ Java2025/August/Code/BOJ_11053.java
@@ -0,0 +1,14 @@
+package Code;
+
+import java.util.*;
+import java.io.*;
+
+class BOJ_11053{
+    public static void main(String[] args) throws Exception{
+        String filepath=System.getProperty("user.dir")+"\\Input\\";
+        int bojNum=11053;
+        BufferedReader br=new BufferedReader(new FileReader(filepath+"input"+bojNum+".txt"));
+
+        br.close();
+    }
+}
