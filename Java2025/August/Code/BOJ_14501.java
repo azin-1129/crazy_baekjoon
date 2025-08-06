c970188670ef544772b948354f4e75ac2f40389d
azin1129 <nuestiloveyou@gmail.com>
2025-08-06 20:34:37 +0900

    Solved+: 14501. 퇴사 바킹독 0x10 14224KB 104ms

--- Java2025/August/Code/BOJ_14501.java
+++ Java2025/August/Code/BOJ_14501.java
@@ -4,16 +4,15 @@ import java.util.*;
 import java.io.*;
 
 class BOJ_14501{
-    static int result=0;
-    static int N;
-    static int[][] reserv;
     public static void main(String[] args) throws Exception{
         String filepath=System.getProperty("user.dir")+"\\Input\\";
         int bojNum=14501;
         BufferedReader br=new BufferedReader(new FileReader(filepath+"input"+bojNum+".txt"));
 
-        N=Integer.parseInt(br.readLine());
-        reserv=new int[N][2];
+        int N=Integer.parseInt(br.readLine());
+        int[][] reserv=new int[N][2];
+        int[] dp=new int[N+1];
+
         StringTokenizer st;
         for(int i=0;i<N;i++){
             st=new StringTokenizer(br.readLine(), " ");
@@ -21,18 +20,14 @@ class BOJ_14501{
             reserv[i][1]=Integer.parseInt(st.nextToken());
         }
 
-        scoring(0, 0);
-        System.out.println(result);
+        for(int i=0;i<N;i++){
+            for(int j=i+reserv[i][0];j<=N;j++){
+                if(dp[j]<(dp[i]+reserv[i][1])){
+                    dp[j]=dp[i]+reserv[i][1];
+                }
+            }
+        }
+        System.out.println(dp[N]);
         br.close();
-    }
-    static void scoring(int idx, int score){
-        if(idx>N){
-            result=Math.max(result, score-reserv[idx][1]);
-            return;
-        }
-
-        for(int i=idx;i<N;i++){
-            scoring(idx+reserv[i][0], score+reserv[i][1]);
-        }
     }
 }
