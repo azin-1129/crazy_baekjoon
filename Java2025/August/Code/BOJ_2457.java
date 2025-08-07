bc1dfd0c2e44ac9559288676a22fd5431ce0c156
azin1129 <nuestiloveyou@gmail.com>
2025-08-07 21:31:50 +0900

    Failed: 2457. 공주님의 정원 바킹독 0x11 백트래킹 시도했으나 .. 특정 날짜가 특정 날짜 범주 사이에 있는지 확인하는 것이 어려움

---
+++ Java2025/August/Code/BOJ_2457.java
@@ -0,0 +1,125 @@
+package Code;
+
+import java.util.*;
+import java.io.*;
+
+class BOJ_2457{
+    static class Flower{
+        int[] bloom;
+        int[] closed;
+
+        public Flower(int bloomMonth, int bloomDay, int closedMonth, int closedDay){
+            this.bloom=new int[]{bloomMonth, bloomDay};
+            this.closed=new int[]{closedMonth, closedDay};
+        }
+
+        @Override
+        public String toString(){
+            return "핀 날:"+bloom[0]+"\\"+bloom[1]+", 진 날:"+closed[0]+"\\"+closed[1];
+        }
+    }
+    static int result=Integer.MAX_VALUE;
+    static int N;
+    static Flower[] flowers;
+    public static void main(String[] args) throws Exception{
+        String filepath=System.getProperty("user.dir")+"\\Input\\";
+        int bojNum=2457;
+        BufferedReader br=new BufferedReader(new FileReader(filepath+"input"+bojNum+".txt"));
+
+        N=Integer.parseInt(br.readLine());
+        flowers=new Flower[N];
+        StringTokenizer st;
+        for(int i=0;i<N;i++){
+            st=new StringTokenizer(br.readLine(), " ");
+            flowers[i]=new Flower(
+                Integer.parseInt(st.nextToken()),
+                Integer.parseInt(st.nextToken()),
+                Integer.parseInt(st.nextToken()),
+                Integer.parseInt(st.nextToken())
+            );
+        }
+
+        for(int i=0;i<N;i++){
+            // 피는 날이 12월 이상: 선택 안함
+            if(flowers[i].bloom[0]>=12) continue;
+            System.out.println("우선 꽃"+i+"를 고릅니다.");
+            choose(i, flowers[i].closed[0], flowers[i].closed[1], 1);
+        }
+
+        System.out.println(result);
+        br.close();
+    }
+    static void choose(int idx, int closedMonth, int closedDay, int count){
+        if(closedMonth==12){
+            result=Math.min(result, count);
+            return;
+        }
+
+        for(int i=(idx+1);i<N;i++){
+            System.out.println(i+"번째 꽃을 고려하고 있습니다.");
+            if(isNextFlowerEnabled(flowers[idx], flowers[i])){
+                System.out.println("다음은 "+i+"꽃입니다.");
+                choose(i, flowers[i].closed[0], flowers[i].closed[1], count+1);
+            }
+        }
+    }
+    static boolean isNextFlowerEnabled(Flower origin, Flower next){
+        int originClosedMonth=origin.closed[0];
+        int originClosedDay=origin.closed[1];
+        int nextFlowerBloomMonth=next.bloom[0];
+        int nextFlowerBloomDay=next.bloom[1];
+        int nextFlowerBloomEndMonth=next.closed[0];
+        int nextFlowerBloomEndDay=next.closed[1]-1;
+
+        if(nextFlowerBloomEndDay==0){ // 이전 월로 교체해야 한다.
+            switch(nextFlowerBloomEndMonth){
+                case 2:
+                    nextFlowerBloomEndDay=31;
+                    break;
+                case 3:
+                    nextFlowerBloomEndDay=28;
+                    break;
+                case 4:
+                    nextFlowerBloomEndDay=31;
+                    break;
+                case 5:
+                    nextFlowerBloomEndDay=30;
+                    break;
+                case 6:
+                    nextFlowerBloomEndDay=31;
+                    break;
+                case 7:
+                    nextFlowerBloomEndDay=30;
+                    break;
+                case 8:
+                    nextFlowerBloomEndDay=31;
+                    break;
+                case 9:
+                    nextFlowerBloomEndDay=31;
+                    break;
+                case 10:
+                    nextFlowerBloomEndDay=30;
+                    break;
+                case 11:
+                    nextFlowerBloomEndDay=30;
+                    break;
+                case 12:
+                    nextFlowerBloomEndDay=30;
+                    break;
+            }
+            nextFlowerBloomEndMonth-=1;
+        }
+
+        System.out.println(origin+" 정보");
+        System.out.println(originClosedMonth+"/"+originClosedDay);
+        System.out.println(next+" 정보");
+        System.out.println(nextFlowerBloomMonth+"/"+nextFlowerBloomDay);
+        System.out.println(nextFlowerBloomEndMonth+"/"+nextFlowerBloomEndDay);
+        // origin 지는 날이 next의 핀 날 이상, 지는 날-1 이하인가?
+        if(originClosedMonth>nextFlowerBloomMonth & originClosedMonth<nextFlowerBloomEndMonth){
+            
+        }
+
+        return false;
+    }
+}
