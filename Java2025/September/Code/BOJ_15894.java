ce396f974fa91716fbddfbc60dc116cbbbee15f3
azin1129 <nuestiloveyou@gmail.com>
2025-09-05 19:58:31 +0900

    Solved: 15894. 수학은 체육 과목입니다 바킹독 0x12 14296KB 100ms

---
+++ Java2025/September/Code/BOJ_15894.java
@@ -0,0 +1,14 @@
+package Code;
+
+import java.util.*;
+import java.io.*;
+
+class BOJ_15894{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        long N=Long.parseLong(br.readLine());
+        System.out.println(4*N);
+        br.close();
+    }
+}
