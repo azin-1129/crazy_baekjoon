ac248091b67d0d077664d32021858de1b5f28baa
azin1129 <nuestiloveyou@gmail.com>
2025-11-11 17:10:32 +0900

    Solved+: 1707 이분 그래프 바킹독 0x18 273052KB 864ms

--- Java2025/November/Code/BOJ_1707.java
+++ Java2025/November/Code/BOJ_1707.java
@@ -2,21 +2,22 @@ import java.util.*;
 import java.io.*;
 
 class BOJ_1707 {
+    static int[] color;
+    static List<List<Integer>> graph;
     public static void main(String[] args) throws Exception {
         String filepath = System.getProperty("user.dir") + "\\Input\\";
         int bojNum = 1707;
         BufferedReader br = new BufferedReader(new FileReader(filepath + "input" + bojNum + ".txt"));
         StringTokenizer st;
-        // 1~N정점을 하나씩 선택하며,
-        // 나머지 정점들이 서로 인접하지 않는지 확인.
-        // 만약 인접하는 정점이 있다면, 실패 처리하고 다음 케이스
-        // 인접 = 선택한 정점을 거치지 않고 이어지는 길이 있는가?
+
         int K=Integer.parseInt(br.readLine());
         for(int k=0;k<K;k++){
-            List<List<Integer>> graph=new ArrayList<>();
+            boolean isBipartite=true;
+            graph=new ArrayList<>();
             st=new StringTokenizer(br.readLine(), " ");
 
             int V=Integer.parseInt(st.nextToken());
+            color=new int[V+1];
             for(int v=0;v<=V;v++){
                 graph.add(new ArrayList<>());
             }
@@ -30,15 +31,43 @@ class BOJ_1707 {
                 graph.get(to).add(from);
             }
 
-            List<Integer> groupA=new ArrayList<>();
-            List<Integer> groupB=new ArrayList<>();
-            for(int a=1;a<=V;a++){ // 정점 그룹 A
-                groupA.add(a);
-                for(int b=(a+1);b<=V;b++){ // 정점 그룹 B
-                    // b의 원소가 a의 원소와 인접한가?
+            // 이분 그래프 알고리즘
+            for(int v=1;v<=V;v++){
+                if(color[v]>0){
+                    continue;
                 }
+
+                color[v]=1;
+                if(!bfs(v)){ // 노드 v와 인접한 노드의 색이 일치하면, 이분 그래프가 아님
+                    isBipartite=false;
+                    break;
+                }
+            }
+            if(isBipartite){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
             }
         }
         br.close();
+    }
+    static boolean bfs(int v){
+        Queue<Integer> q=new ArrayDeque<>();
+        q.offer(v);
+        while(!q.isEmpty()){
+            int current=q.poll();
+            int nextColor=color[current]%2+1;
+
+            for(int next : graph.get(current)){
+                if(color[next]==0){ // 미방문
+                    color[next]=nextColor;
+                    q.offer(next);
+                }else if(color[next]!=nextColor){ // 인접 노드와 현재 노드의 색상이 같음(이분 X)
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
