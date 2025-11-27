9aebc95983cf939a7af820c063d7d4cb80b3dcc8
azin1129 <nuestiloveyou@gmail.com>
2025-11-27 20:58:22 +0900

    Failed: 2250. 트리의 높이와 너비 바킹독 0x19 NullPointerException(1%)

--- Java2025/November/Code/BOJ_2250.java
+++ Java2025/November/Code/BOJ_2250.java
@@ -14,7 +14,7 @@ class BOJ_2250 {
 
         @Override
         public String toString(){
-            return idx+"번 노드입니다.";
+            return "{idx:"+idx+", col:"+col+"}";
         }
     }
     static class BST{
@@ -25,10 +25,10 @@ class BOJ_2250 {
         }
 
         void init(int idx, int left, int right){
-            System.out.println(idx+"번 노드의 자녀:"+left+", "+right);
-            System.out.println("****"+idx+"노드가 있는지 탐색합니다..");
+            // System.out.println(idx+"번 노드의 자녀:"+left+", "+right);
+            // System.out.println("****"+idx+"노드가 있는지 탐색합니다..");
             Node node=find(this.root, idx);
-            System.out.println("노드를 찾았어요."+node);
+            // System.out.println("노드를 찾았어요."+node);
             if(left!=-1){
                 node.left=new Node(left);
             }
@@ -38,18 +38,27 @@ class BOJ_2250 {
         }
 
         Node find(Node node, int idx){
-            System.out.println(idx+"번을 찾는중..");
-            System.out.println("현재:"+node);
+            // System.out.println(idx+"번을 찾는중..");
+            // System.out.println("현재:"+node);
             // if x<y : less than 0
             // if x>y : greater than 0
-            int compResult=Integer.compare(node.idx, idx);
-            if(compResult<0){
-                return find(node.right, idx);
-            }else if(compResult>0){
-                return find(node.left, idx);
-            }else{
-                return node;
+            Queue<Node> q=new ArrayDeque<>();
+            q.offer(node);
+            while(!q.isEmpty()){
+                Node current=q.poll();
+                // System.out.println(current);
+                if(current.idx==idx){
+                    return current;
+                }
+                if(current.left!=null){
+                    q.offer(current.left);
+                }
+                if(current.right!=null){
+                    q.offer(current.right);
+                }
             }
+
+            return null;
         }
 
         void inOrder(int level){
@@ -91,8 +100,17 @@ class BOJ_2250 {
         // 열 순서 매기고, 레벨 정보 계산
         bst.inOrder(1);
 
-        int minimumLevel=Integer.MAX_VALUE;
-        int maximumLength=0;
+        // System.out.println(levels);
+        PriorityQueue<int[]> pq=new PriorityQueue<>(new Comparator<>(){
+            @Override
+            public int compare(int[] arr1, int[] arr2){
+                if(arr1[1]==arr2[1]){
+                    return Integer.compare(arr1[0], arr2[0]);
+                }else{
+                    return Integer.compare(arr2[1], arr1[1]);
+                }
+            }
+        });
         for(int lv=1;lv<=N;lv++){
             List<Node> nodes=levels.get(lv);
             int size=nodes.size();
@@ -100,15 +118,15 @@ class BOJ_2250 {
                 break;
             }
             int length=(nodes.get(size-1).col-nodes.get(0).col)+1;
-            if(maximumLength<=length){
-                if(minimumLevel>lv){
-                    minimumLevel=lv;
-                }
-                maximumLength=length;
-            }
+            // System.out.println(lv+"층의 폭:"+length);
+            int[] temp=new int[2];
+            temp[0]=lv;
+            temp[1]=length;
+            pq.offer(temp);
         }
 
-        System.out.println(minimumLevel+" "+maximumLength);
+        int[] result=pq.poll();
+        System.out.println(result[0]+" "+result[1]);
         br.close();
     }
 }
