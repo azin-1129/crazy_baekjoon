dd99229adb9e4e68e590b714fe0e8610ad37ff50
azin1129 <nuestiloveyou@gmail.com>
2025-12-08 19:28:58 +0900

    Chore: 2025년 12월 디렉터리 추가

---
+++ Java2025/December/Code/BOJ_2250.java
@@ -0,0 +1,14 @@
+package Code;
+
+import java.util.*;
+import java.io.*;
+
+class BOJ_2250 {
+    public static void main(String[] args) throws Exception {
+        String filepath = System.getProperty("user.dir") + "\\Input\\";
+        int bojNum=2250;
+        BufferedReader br = new BufferedReader(new FileReader(filepath + "input" + bojNum + ".txt"));
+
+        br.close();
+    }
+}
