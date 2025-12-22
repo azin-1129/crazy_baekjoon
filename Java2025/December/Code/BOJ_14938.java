ecade5f99e7e504bd4e3ba1729d78546e79eae22
azin1129 <nuestiloveyou@gmail.com>
2025-12-22 12:36:05 +0900

    Solved: 14938. 서강그라운드 바킹독 0x1C 14772KB 136ms

--- Java2025/December/Code/BOJ_14938.java
+++ Java2025/December/Code/BOJ_14938.java
@@ -4,19 +4,6 @@ import java.util.*;
 import java.io.*;
 
 public class BOJ_14938 {
-    static class Node{
-        int current;
-        int dist;
-
-        Node(int current, int dist){
-            this.current=current;
-            this.dist=dist;
-        }
-    }
-    static List<List<Integer>> graph=new ArrayList<>();
-    static int[][] floyd;
-    static int[] itemCounts;
-    static int N, M;
     public static void main(String[] args) throws Exception{
         String filepath=System.getProperty("user.dir")+"\\Input\\";
         int bojNum=14938;
@@ -25,23 +12,18 @@ public class BOJ_14938 {
 
         // 답: 범위 m 이내에서 획득할 수 있는 아이템 양의 최대치
         st=new StringTokenizer(br.readLine(), " ");
-        N=Integer.parseInt(st.nextToken()); // 노드 수
-        M=Integer.parseInt(st.nextToken()); // 수색 범위
+        int N=Integer.parseInt(st.nextToken()); // 노드 수
+        int M=Integer.parseInt(st.nextToken()); // 수색 범위
         int R=Integer.parseInt(st.nextToken()); // 엣지 수
 
-        itemCounts=new int[N+1];
+        int[] itemCounts=new int[N+1];
         st=new StringTokenizer(br.readLine(), " ");
         for(int i=0;i<N;i++){
             itemCounts[i+1]=Integer.parseInt(st.nextToken());
         }
 
-        // 그래프 초기화
-        for(int i=0;i<=N;i++){
-            graph.add(new ArrayList<>());
-        }
-
         // 플로이드 배열 정보 초기화
-        floyd=new int[N+1][N+1];
+        int[][] floyd=new int[N+1][N+1];
         for(int i=1;i<=N;i++){
             Arrays.fill(floyd[i], 16);
             floyd[i][i]=0; // 자기 자신까지는 0
@@ -54,8 +36,6 @@ public class BOJ_14938 {
             // 양방향
             floyd[from][to]=weight;
             floyd[to][from]=weight;
-            graph.get(from).add(to);
-            graph.get(to).add(from);
         }
 
         // 양방향 그래프 distance 확보(플로이드 워셜)
@@ -74,44 +54,21 @@ public class BOJ_14938 {
             }
             System.out.println();
         }
-        
+
         // 1~n번 시작점부터 낙하, m 이내 노드 아이템 획득(visited 처리)
         int result=0;
-        for(int i=1;i<=N;i++){
-            result=Math.max(result, bfs(i));
+        for(int from=1;from<=N;from++){
+            int cost=0;
+            for(int to=1;to<=N;to++){
+                if(floyd[from][to]>M){
+                    continue;
+                }
+                cost+=itemCounts[to];
+            }
+            result=Math.max(result, cost);
         }
 
         System.out.println(result);
         br.close();
-    }
-    static int bfs(int start){
-        System.out.println("시작점: "+start);
-        boolean[] visited=new boolean[N+1];
-        Queue<Node> q=new ArrayDeque<>();
-        q.offer(new Node(start, 0));
-
-        int weight=0;
-        while(!q.isEmpty()){
-            Node node=q.poll();
-            int current=node.current;
-            int dist=node.dist;
-            if(visited[current]){
-                continue;
-            }
-            visited[current]=true;
-            weight+=itemCounts[current];
-            System.out.println(current+"를 방문, 지금까지의 거리는 "+dist);
-            for(int next:graph.get(current)){
-                int nextDist=dist+floyd[current][next];
-                if(nextDist>M){
-                    System.out.println(current+" to "+next+"는 고려하지 않습니다.");
-                    continue;
-                }
-                q.offer(new Node(next, nextDist));
-            }
-        }
-
-        System.out.println();
-        return weight;
     }
 }
